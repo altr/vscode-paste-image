4aaa470a478b163c225002c28f2cb1b9aea8bc8f
Al <alain@altr.ch>
2024-09-27 16:12:38 +1000

    Update copy command

--- res/linux_cinammon.sh
+++ res/linux_cinammon.sh
@@ -6,9 +6,10 @@ command -v xclip >/dev/null 2>&1 || { echo >&1 "no xclip"; exit 1; }
 # write image in clipboard to file (see http://unix.stackexchange.com/questions/145131/copy-image-from-clipboard-to-file)
 if
 #xclip -selection clipboard -target image/png -o >/dev/null 2>&1
-find ~/Pictures/ -type f -newermt '30 seconds ago' -name "*.png" | tail -1 >/dev/null 2>&1
+ls -tr ~/Pictures/*.png | tail -1 >/dev/null 2>&1
 then
-find ~/Pictures/ -type f -newermt '30 seconds ago' -name "*.png" | tail -1
+cp "$(ls -tr ~/Pictures/*.png | tail -1)" "$1"
+echo $1
 else
 echo "no image"
 fi
